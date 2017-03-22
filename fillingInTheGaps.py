eb1468058d4565d1fe0d81414f33430c3c007f03
RandyRomero <a.ya.miheev@gmail.com>
2017-03-22 12:17:35 +0300

    write loop that looks for missing files

--- fillingInTheGaps.py
+++ fillingInTheGaps.py
@@ -1,1 +1,12 @@
-#! pyhton3
+#! python3
+
+import os
+
+pathToWork = ('.\\test')
+
+files = os.listdir(pathToWork)
+
+# look for missing files
+for i in range(1, len(files) + 1):
+	if not os.path.exists(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i))):
+		print('file{0:0>3}.txt is missing!'.format(i))
