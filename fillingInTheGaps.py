1281b571e5df5814572c541eeeb3b92811b7e04a
RandyRomero <a.ya.miheev@gmail.com>
2017-03-22 17:51:39 +0300

    add draft of a algorithm for filling the gaps if some files is missing

--- fillingInTheGaps.py
+++ fillingInTheGaps.py
@@ -11,6 +11,24 @@ for i in range(1, len(files) + 1):
 	if not os.path.exists(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i))):
 		print('file{0:0>3}.txt is missing!'.format(i))
 		allesGut = False
+		y = 1
+		while True: 
+			if os.path,exists(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i + y)))
+				#rename
+				break
+			else:
+				if not y > len(files)
+					y += 1
+					continue
+				else:
+					print('There are no files to rename anymore')
 
+		
 if allesGut:
 	print('All files are there.')
+
+
+
+# if os.path.exists(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i + 1))):
+# 			#rename file to previous
+# 		else:	
