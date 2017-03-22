65ae431c4ca0cca40146cb4344a6c7f8930047a6
RandyRomero <a.ya.miheev@gmail.com>
2017-03-22 12:20:26 +0300

    print message if all files are there

--- fillingInTheGaps.py
+++ fillingInTheGaps.py
@@ -3,10 +3,14 @@
 import os
 
 pathToWork = ('.\\test')
-
 files = os.listdir(pathToWork)
+allesGut = True
 
 # look for missing files
 for i in range(1, len(files) + 1):
 	if not os.path.exists(os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i))):
 		print('file{0:0>3}.txt is missing!'.format(i))
+		allesGut = False
+
+if allesGut:
+	print('All files are there.')
