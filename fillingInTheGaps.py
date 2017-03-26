6342f47f973a4abf1fe6a487a28fe23d7f7694df
RandyRomero <a.ya.miheev@gmail.com>
2017-03-26 19:04:54 +0300

    Significantly change some parts: add logging through logging standart module; make script start count from first serial number whatever it is, not only from 1; make script evaluate which file is last in order no to try look for files with greater serial number if there are no such files

--- fillingInTheGaps.py
+++ fillingInTheGaps.py
@@ -2,18 +2,44 @@
 
 
 '''Program that finds all files with a given prefix, such as spam001.txt, spam002.txt, and so on, in a single folder and locates any gaps in the numbering (such as if there is a spam001.txt and spam003.txt but no spam002.txt). Have the program rename all the later files to close this gap.'''
-import os
+
+import os, logging, re
+
+logging.basicConfig(
+    format = "%(levelname) -1s %(asctime)s line %(lineno)s: %(message)s",
+    level = logging.DEBUG
+)
 
 pathToWork = ('.\\test')
-files = os.listdir(pathToWork)
 allesGut = True
 
+
+def searchBoundaries():
+	files = os.listdir(pathToWork)
+	# gather only files with right mask
+	files[:] = [file for file in files if re.search(r'file(\d{3})\.txt', file)]
+	# gather file serial number of these files
+	serialNumbers = []
+	for file in files:
+		serialNum = re.search(r'file(\d{3}).txt', file).group(1)
+		serialNumbers.append(serialNum)
+
+	serialNumbers.sort()		
+	firstNumber = serialNumbers[0]
+	lastNumber = serialNumbers[len(serialNumbers)-1]
+	logging.debug(lastNumber)
+
+	return files, int(firstNumber), int(lastNumber)
+
+files, firstNumber, lastNumber = searchBoundaries()
+
 # look for missing files
-for i in range(1, len(files) + 1):
+for i in range(firstNumber, len(files) + 1):
 	currentFile = os.path.join(pathToWork, 'file{0:0>3}.txt'.format(i))
 	if not os.path.exists(currentFile):
 		print('file{0:0>3}.txt is missing!'.format(i))
 		allesGut = False
+		
 		y = 1
 
 		while True:
@@ -28,8 +54,9 @@ for i in range(1, len(files) + 1):
 				break
 			else:
 				# if file doesn't exist - increment 'y' in order to look for the file with bigger ordinal number next time
-				if not y > len(files):
+				if i + y < lastNumber:
 					y += 1
+					logging.debug(y)
 					continue
 				else:
 					print('There are no files to rename anymore')
